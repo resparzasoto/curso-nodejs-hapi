5605d1c0a60eb3dac88873ea2bb228a35a414d73
resparzasoto <rubenesparzasoto95@gmail.com>
2020-08-22 14:47:39 -0600

    Renderizado de vistas - Layout y template del home

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
+const handlerbars = require('handlebars')
 const inert = require('@hapi/inert')
 const path = require('path')
+const vision = require('@hapi/vision')
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
@@ -17,12 +19,25 @@ const server = Hapi.server({
 async function init () {
   try {
     await server.register(inert)
+    await server.register(vision)
+
+    server.views({
+      engines: {
+        hbs: handlerbars
+      },
+      relativeTo: __dirname,
+      path: 'views',
+      layout: true,
+      layoutPath: 'views'
+    })
 
     server.route({
       method: 'GET',
-      path: '/home',
+      path: '/',
       handler: (req, h) => {
-        return h.file('index.html')
+        return h.view('index', {
+          title: 'home'
+        })
       }
     })
 
