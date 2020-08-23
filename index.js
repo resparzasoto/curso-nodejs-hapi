eb98ef830112ff6532bf4e71c0a5dca3600801bb
resparzasoto <rubenesparzasoto95@gmail.com>
2020-08-23 09:08:33 -0600

    Definir una mejor estructura con buenas prácticas en Hapi

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
-const handlerbars = require('handlebars')
 const inert = require('@hapi/inert')
-const path = require('path')
 const vision = require('@hapi/vision')
+const handlerbars = require('handlebars')
+const path = require('path')
+const routes = require('./routes')
 
 const server = Hapi.server({
   port: process.env.PORT || 3000,
@@ -31,45 +32,7 @@ async function init () {
       layoutPath: 'views'
     })
 
-    server.route({
-      method: 'GET',
-      path: '/',
-      handler: (req, h) => {
-        return h.view('index', {
-          title: 'home'
-        })
-      }
-    })
-
-    server.route({
-      method: 'GET',
-      path: '/register',
-      handler: (req, h) => {
-        return h.view('register', {
-          title: 'Registro'
-        })
-      }
-    })
-
-    server.route({
-      method: 'POST',
-      path: '/create-user',
-      handler: (req, h) => {
-        console.log(req.payload)
-        return 'Usuario creado'
-      }
-    })
-
-    server.route({
-      method: 'GET',
-      path: '/{param*}',
-      handler: {
-        directory: {
-          path: '.',
-          index: ['index.html']
-        }
-      }
-    })
+    server.route(routes)
 
     await server.start()
   } catch (error) {
